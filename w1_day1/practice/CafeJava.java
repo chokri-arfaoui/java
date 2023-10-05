3afb48140d73eb1f08abbc28a5342296110eda0b
arfaoui chokri <arfaouichokri601@gmail.com>
2023-10-05 11:10:44 +0100

    practices added

---
+++ w1_day1/practice/CafeJava.java
@@ -0,0 +1,55 @@
+public class CafeJava {
+    public static void main(String[] args) {
+        // APP VARIABLES
+        // Lines of text that will appear in the app. 
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = "Your total is $";
+        
+        // Menu variables (add yours below)
+        double mochaPrice = 3.5;
+        double dripCoffeePrice = 1.5;
+        double lattePrice = 2.2;
+        double cappuccinoPrice = 3.2;
+
+
+    
+        // Customer name variables (add yours below)
+        String customer1 = "Cindhuri";
+        String customer2 = "Sam";
+        String customer3 = "Jimmy";
+        String customer4 = "Noah";
+    
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = false;
+        boolean isReadyOrder2 =false;
+        boolean isReadyOrder3 = true;
+        boolean isReadyOrder4 = true;
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        // System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+    	// // ** Your customer interaction print statements will go here ** //
+        // System.out.println(pendingMessage );
+        // System.out.println(customer1 + pendingMessage);
+        // if (isReadyOrder4=true){
+        //      System.out.println(customer4 + readyMessage);
+        //      System.out.println(displayTotalMessage + cappuccinoPrice);
+        // }else{
+        //          System.out.println(customer4 + pendingMessage);
+        //     }
+        
+        // System.out.println(customer2   +   displayTotalMessage + lattePrice * 2 );
+        // if (isReadyOrder2){
+        //      System.out.println(customer2 + readyMessage);
+        //      System.out.println(displayTotalMessage + lattePrice * 2 );
+        // }else{
+        //          System.out.println(customer2 + pendingMessage);
+        // //     
+        // }
+        
+            
+
+        System.out.println(customer3  + displayTotalMessage + (lattePrice - dripCoffeePrice));
+    }
+}
---
+++ w1_day1/practice/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+public class FirstJavaProgram {
+     public static void main (String[] args)
+     {
+     String a = "My Name Is : Arfaoui Chokri \n" ;
+     String b =" I am 49 years \n";
+     String c ="My Hometown Is : Tunis \n";
+     System.out.println(a+b+c);
+     }
+}
